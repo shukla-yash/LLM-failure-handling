7425952d3aca8350fa664580eb5973bc624dfca8
WenchangGaoT <wenchang.gao@tufts.edu>
2024-09-25 21:27:29 -0400

    added language api

--- apis/gpt4_apis.py
+++ apis/gpt4_apis.py
@@ -97,6 +97,48 @@ def get_vision_response(system_prompts, user_prompts: list, images: str, api_tok
 
     return response.json()['choices'][0]['message']['content']
 
+def get_language_response(system_prompts, user_prompts: list, api_token: str):
+    user_prompt = build_prompts(user_prompts) 
+    # print('user prompt: ', user_prompt)
+    system_prompt = build_system_prompts(system_prompts) 
+    # print('system prompt: ', system_prompt)
+    # client = OpenAI() 
+
+    base64_image = encode_image('images/' + images) 
+    headers = {
+      "Content-Type": "application/json",
+      "Authorization": f"Bearer {api_token}"
+    }
+
+    payload = {
+      'model': "gpt-4-turbo",
+      'messages': [
+        {
+          "role": "user",
+          "content": [
+            {
+                "type": "text", 
+                "text": user_prompt
+            },
+          ],
+        }, 
+        {
+            'role': 'system', 
+            'content': [
+                {
+                    'type': 'text', 
+                    'text': system_prompt
+                }
+          ]
+        }
+      ],
+      'max_tokens':300,
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    return response.json()['choices'][0]['message']['content']
+
 if __name__ == '__main__':
     system_prompts = ['sys_test.txt']
     user_prompts = ['test.txt'] 
